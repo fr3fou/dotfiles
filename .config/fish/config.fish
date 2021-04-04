7b207be180e58680390f25ba3d1319a0083a5076
Simo Aleksandrov <simo3003@me.com>
2021-04-04 17:14:05 +0300

    add $ANDROID_SDK_ROOT and related bin path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,13 @@
 set -x GOPATH $HOME/.go
 set -x EDITOR nvim
+set -x ANDROID_SDK_ROOT /opt/android-sdk
 
 if begin; status --is-interactive; and test "$COLORTERM" = xfce4-terminal ; end
     set -gx TERM xterm-256color
 end
 
-fish_add_path $GOPATH/bin
 fish_add_path $HOME/.bin
+fish_add_path $GOPATH/bin $HOME/.yarn/bin 
+fish_add_path $ANDROID_SDK_ROOT/tools/bin $ANDROID_SDK_ROOT/platform-tools $ANDROID_SDK_ROOT/build-tools/30.0.3 $ANDROID_SDK_ROOT/emulator
 
 thefuck --alias | source
