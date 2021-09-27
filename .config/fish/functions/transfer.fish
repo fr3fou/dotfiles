6c3bc2c61101312ab437b767090df718dd4582d8
Simo Aleksandrov <simo3003@me.com>
2021-09-27 22:01:16 +0300

    transfer script

--- .config/fish/functions/transfer.fish
+++ .config/fish/functions/transfer.fish
@@ -1,11 +1,11 @@
-function t --description 'Upload a file to cdn.simo.sh'
+function transfer --description 'Upload a file to transfer.sh'
     if [ $argv[1] ]
         # write to output to tmpfile because of progress bar
         set -l tmpfile ( mktemp -t transferXXXXXX )
         curl --progress-bar --upload-file "$argv[1]" https://cdn.simo.sh/(basename $argv[1]) >> $tmpfile
-        cat $tmpfile | pbcopy; cat $tmpfile
+        cat $tmpfile
         command rm -f $tmpfile
     else
-        echo 'usage: t FILE_TO_TRANSFER'
+        echo 'usage: transfer FILE_TO_TRANSFER'
     end
 end
