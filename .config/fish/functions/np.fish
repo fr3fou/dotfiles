9d804388dea9049f0abeea1df223ea08e9716026
Simo Aleksandrov <simo3003@me.com>
2021-10-08 19:05:54 +0300

    feat: improve np.fish to support generating a random name when reading from stdin / piping

--- .config/fish/functions/np.fish
+++ .config/fish/functions/np.fish
@@ -2,7 +2,11 @@ function np --description 'Upload a file to cdn.simo.sh'
     if [ $argv[1] ]
         # write to output to tmpfile because of progress bar
         set -l tmpfile ( mktemp -t transferXXXXXX )
-        curl --progress-bar --upload-file "$argv[1]" https://cdn.simo.sh/(basename $argv[1]) >> $tmpfile
+        set -l name ( basename $argv[1] )
+        if [ $argv[1] = '-' ]
+            set name ( openssl rand -hex 4 )
+        end
+        curl --progress-bar --upload-file "$argv[1]" https://cdn.simo.sh/$name >> $tmpfile
         cat $tmpfile | pbcopy; cat $tmpfile
         command rm -f $tmpfile
     else
